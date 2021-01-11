97bd7c376f112fb7b6e273c66b9b604fed1f5c90
incals_ubuntu <kush373@gmail.com>
2021-01-12 00:09:03 +0530

    app.dart bottom navigation added

---
+++ lib/src/app.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'ui/country_list.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    CountryList(),
+    CountryList(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          this.setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.list_alt_outlined,
+                color: Colors.red,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+                color: Colors.red,
+              ),
+              label: 'Favorite'),
+        ],
+      ),
+    );
+  }
+}
